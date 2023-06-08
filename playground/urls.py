fef1d0e619fdbbee116f9eaadfebfa9156ea60f6
iadamsaleh <135939374+iadamsaleh@users.noreply.github.com>
2023-06-08 10:40:05 +0500

    Update urls.py

--- playground/urls.py
+++ playground/urls.py
@@ -4,7 +4,7 @@ from . import views
 #url configration
 urlpatterns=[
     #path('hello/',views.say_hello)
-    path('',views.say_hello, name='home'),
+    path('',views.index, name='home'),
     path('index',views.index, name='index'),
     path('about',views.about, name='about'),
     path('Signup',views.signup, name='signup'),
